bbcc397d659cdfcfd18842086e5030a3567974ac
ChenlingZhang <Chenling_Zhang@hotmail.com>
2022-04-20 22:13:27 +0800

    SE02

---
+++ SE02/src/Day01/FileDemo.java
@@ -0,0 +1,12 @@
+package Day01;
+
+import java.io.File;
+
+public class FileDemo {
+    public static void main(String[] args) {
+        File file = new File("./Demo.txt");
+        System.out.println(file.length());
+
+    }
+
+}
