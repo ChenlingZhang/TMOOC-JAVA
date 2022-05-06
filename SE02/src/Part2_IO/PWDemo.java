931d530b76dcb6c367e6adca90816596575fed4c
ChenlingZhang <Chenling_Zhang@hotmail.com>
2022-05-06 21:52:06 +0800

    IO

--- SE02/src/Part2_IO/PWDemo.java
+++ SE02/src/Part2_IO/PWDemo.java
@@ -1,4 +1,14 @@
 package Part2_IO;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 public class PWDemo {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter pw = new PrintWriter("./pw.txt","UTF-8");
+        pw.println("一壶清茶一包烟，一行代码想一天");
+        pw.println("这是第二行");
+        pw.close();
+    }
 }
---
+++ SE02/src/Part2_IO/PWDemo02.java
@@ -0,0 +1,34 @@
+package Part2_IO;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * 在流链接中使用pw
+ */
+public class PWDemo02 {
+    public static void main(String[] args) throws IOException {
+        // 文件字节输出流，向文件中写入字节数据
+        FileOutputStream fos = new FileOutputStream("./pw2.txt", true);
+        // 转换输出流， 1. 衔接字符与字节流 2. 将写出的字符转换成字节
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        // 缓冲字符输出流，快滴血加快效率
+        BufferedWriter bw = new BufferedWriter(osw);
+        PrintWriter writer = new PrintWriter(bw);
+        Scanner in = new Scanner(System.in);
+        boolean isContinue = true;
+        while (isContinue){
+            System.out.println("Please Enter content, enter exit to close: ");
+            String content = in.nextLine();
+            if (content.equalsIgnoreCase("exit")){
+                isContinue = false;
+            }
+            else {
+                writer.println(content);
+                writer.flush();
+            }
+
+        }
+        writer.close();
+    }
+}
