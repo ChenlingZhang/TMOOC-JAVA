6e0640c93526fdc7bfd4285bdb83406929447ce2
ChenlingZhang <Chenling_Zhang@hotmail.com>
2022-06-01 21:34:59 +0800

    WebServer

--- VN2202WebServer/V9/src/main/java/core/ClientHandle.java
+++ VN2202WebServer/V9/src/main/java/core/ClientHandle.java
@@ -26,29 +26,9 @@ public class ClientHandle implements Runnable {
             // 响应请求
             HttpServletResponse response = new HttpServletResponse(socket);
             // 处理请求
-            String path = request.getUrl();
-            System.out.println("Abstract Path: " + path);
-
-            File static_dir = new File(ClientHandle.class.getClassLoader().getResource("./static").toURI());
-            /*
-            指定父级目录，防止子级目录报错，影响父级目录。报空指针错误
-             */
-            File file = new File(static_dir,path);
-            if (file.isFile()){ // 如果是文件，说明访问的资源存在
-                // 发送状态行
-                response.setContentFile(file);
-
-            }
-            else{
-                response.setStatusCode(404);
-                response.setStatusResult("NotFound");
-                // 如果不存在则访问404页面
-                file = new File(static_dir,"/root/404.html");
-                response.setContentFile(file);
-
-            }
+            DispatcherServlet servlet = new DispatcherServlet();
+            servlet.service(request,response);
             // 发送响应
-
             response.response();
 
             System.out.println("All content has been send");
---
+++ VN2202WebServer/V9/src/main/java/core/DispatcherServlet.java
@@ -0,0 +1,41 @@
+package core;
+
+import http.HTTPServletRequest;
+import http.HttpServletResponse;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+public class DispatcherServlet {
+    private static File staticDir;
+
+    static {
+        try {
+            staticDir = new File(ClientHandle.class.getClassLoader().getResource("./static").toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void service(HTTPServletRequest request, HttpServletResponse response) {
+        String path = request.getUrl();
+        System.out.println("Abstract Path: " + path);
+        /*
+          指定父级目录，防止子级目录报错，影响父级目录。报空指针错误
+         */
+        File file = new File(staticDir,path);
+        if (file.isFile()){ // 如果是文件，说明访问的资源存在
+            // 发送状态行
+            response.setContentFile(file);
+
+        }
+        else{
+            response.setStatusCode(404);
+            response.setStatusResult("NotFound");
+            // 如果不存在则访问404页面
+            file = new File(staticDir,"/root/404.html");
+            response.setContentFile(file);
+
+        }
+    }
+}
