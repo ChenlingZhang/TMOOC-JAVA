be115936e60c2b62d9d20213f6c24bc4a3f74e40
ChenlingZhang <Chenling_Zhang@hotmail.com>
2022-08-29 22:30:14 +0800

    Mybatis

--- CGBVN2202Session4/src/test/java/cn/tedu/csmall/sever/BrandTest.java
+++ CGBVN2202Session4/src/test/java/cn/tedu/csmall/sever/BrandTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 @SpringBootTest
 public class BrandTest {
     @Autowired
@@ -27,5 +30,12 @@ public class BrandTest {
         System.out.println("业务层" + brandController.brandService);
         System.out.println("数据访问层："+brandRepository);
         System.out.println("数据访问层："+brandServiceImpl.brandRepository);
+    }
+    @Autowired
+    DataSource dataSource;
+    @Test
+    public void testMysqlConnection() throws SQLException {
+        dataSource.getConnection();
+        System.out.println("properites配置成功！");
     }
 }
